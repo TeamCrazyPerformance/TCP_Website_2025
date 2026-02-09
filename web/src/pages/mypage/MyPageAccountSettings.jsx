6703bd97cb5faa5dc076a6974b58e26569076164
junsulee <junsulee119@gmail.com>
2026-02-09 22:50:06 +0900

    fix: fix duplicated 비밀번호 변경

--- web/src/pages/mypage/MyPageAccountSettings.jsx
+++ web/src/pages/mypage/MyPageAccountSettings.jsx
@@ -133,11 +133,6 @@ const MyPageAccountSettings = () => {
                 </div>
             </form>
 
-            <div className="mt-6 flex justify-end">
-                <button type="button" onClick={() => setIsModalOpen(true)} className="px-4 py-2 rounded-lg btn-outline text-red-400 hover:bg-red-400/10 border-red-400">
-                    <i className="fa-solid fa-key mr-2"></i>비밀번호 변경
-                </button>
-            </div>
             <PasswordChangeModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
         </div>
     );
