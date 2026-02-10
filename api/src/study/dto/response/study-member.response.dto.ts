132b05f581a69f80dbbb12fc66474c9e71655756
junsulee <junsulee119@gmail.com>
2026-02-10 14:26:51 +0900

    fix: study ui update

--- api/src/study/dto/response/study-member.response.dto.ts
+++ api/src/study/dto/response/study-member.response.dto.ts
@@ -4,4 +4,8 @@ export class StudyMemberResponseDto {
   user_id: string;
   name: string;
   role: StudyMemberRole; // PENDING, MEMBER, LEADER
+  profile_image?: string | null;
+  major?: string | null;
+  self_description?: string | null;
+  intro?: string | null;
 }
